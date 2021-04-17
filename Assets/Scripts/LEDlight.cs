d1df0885998db631ef34acba8f846bd50e076b8d
Basshi <Kurorie96@outlook.jp>
2021-04-17 16:48:52 +0900

    指定のキーを押すとLEDが光るようになる

---
+++ Assets/Scripts/LEDlight.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LEDlight : MonoBehaviour
+{
+
+    public SerialHandler serialHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            serialHandler.Write("0");
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            serialHandler.Write("1");
+        }
+    }
+
+    void OnDataReceived(string message)
+    {
+        var data = message.Split(
+                new string[] { "\n" }, System.StringSplitOptions.None);
+        if (data.Length < 2) return;
+
+        try
+        {
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+
+}
