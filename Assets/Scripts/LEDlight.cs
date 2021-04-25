7b87b3a05b3c92d77082dfbfd6570d40e24878a2
Basshi <Kurorie96@outlook.jp>
2021-04-26 07:16:57 +0900

    シリアル通信に関するものをオブジェクトから取った
    ここからブランチをわける

--- Assets/Scripts/LEDlight.cs
+++ Assets/Scripts/LEDlight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LEDlight : MonoBehaviour
+public class LedLight : MonoBehaviour
 {
 
     public SerialHandler serialHandler;
--- Assets/Scripts/SerialHandler.cs
+++ Assets/Scripts/SerialHandler.cs
@@ -13,7 +13,7 @@ public class SerialHandler : MonoBehaviour
     //Linuxでは/dev/ttyUSB0
     //windowsではCOM1
     //Macでは/dev/tty.usbmodem1421など
-    public string portName = "/dev/cu.usbmodem141101";
+    public string portName = "COM3";
     public int baudRate = 9600;
 
     private SerialPort serialPort_;
